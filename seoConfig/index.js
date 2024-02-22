69410775f0a0fd86c4f87854998c73b8d55d7f87
DjTer3a <mostafa@higazy.dev>
2024-02-22 08:44:44 +0200

    porting articles

---
+++ seoConfig/index.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Red City",
+  description:
+    "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+  og: {
+    title: "Red City",
+    description:
+      "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+    image: "https://logspot.vercel.app/logspot-banner.png",
+    url: "https://logspot.vercel.app",
+  },
+};
